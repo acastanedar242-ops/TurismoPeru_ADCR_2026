5481841d3d88c8db6ac6f9823be8038c5dd85afb
Señor X <acastanedar_24_2@unc.edu.pe>
2026-08-14 12:06:18 -0500

    Funcion Tabla Multiples Sentencias: 8.fn_mt_ReservaCliente

---
+++ 2.Fuctions/8.fn_mt_ReservaCliente.sql
@@ -0,0 +1,69 @@
+--Sintaxis Funcion Tabla Multiples Sentencias
+
+CREATE OR ALTER FUNCTION ADCR.FN_NOMBREFUNCION
+(
+	--@parametro TipoDato
+	@edad INT
+)
+RETURNS @Resultado TABLE
+(
+	Columna 1 tipoDato,
+	Columna 2 tipoDato,
+	Columna 3 tipoDato,
+)
+AS
+BEGIN
+	--Istrucciones SQL
+	RETURN;
+END;
+GO
+
+--Reservas de un cliente
+CREATE OR ALTER FUNCTION ADCR.fn_MT_ReservasClientes
+(
+	@IdCliente INT
+)
+RETURNS @Resultado TABLE
+(
+	IdReserva int,
+	FechaReserva date,
+	EstadoReserva varchar(100),
+	TotalPagado money
+)
+AS
+BEGIN
+	INSERT INTO @Resultado
+	(
+		IdReserva,
+		FechaReserva,
+		EstadoReserva,
+		TotalPagado
+	)
+	SELECT
+		R.id_reserva,
+		R.fecha_reserva,
+		ER.nombre, --Viene de estado de la reserva
+		ISNULL (SUM(P.monto), 0) -- viene de pagos
+	FROM ADCR.reserva R 
+	INNER JOIN 
+		ADCR.estado_reserva ER on
+		ER.id_estado_reserva = R.id_estado_reserva
+	LEFT JOIN 
+		ADCR.pago P on
+		R.id_reserva = P.id_reserva
+	WHERE R.id_cliente = @IdCliente
+
+	GROUP BY
+		R.id_reserva,
+		R.fecha_reserva,
+		ER.nombre; --Estado Reserva
+
+	RETURN;
+END;
+GO
+
+--ejecucion
+Select *,
+	GETDATE() as Fecha_Consulta,
+	ADCR.fn_NombreCompletoPersona(105) as Estudiante
+FROM ADCR.fn_ReservaCliente(10);

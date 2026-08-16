f07ce485da209bc3b1263af3a59a7f31683fcb04
Señor X <acastanedar_24_2@unc.edu.pe>
2026-08-15 20:07:12 -0500

    Funcion Tabla Multiples Sentencias: 8.fn_mt_ReservaCliente

--- 2.Fuctions/8.fn_mt_ReservaCliente.sql
+++ 2.Fuctions/8.fn_mt_ReservaCliente.sql
@@ -1,23 +1,3 @@
---Sintaxis Funcion Tabla Multiples Sentencias
-
-CREATE OR ALTER FUNCTION ADCR.FN_NOMBREFUNCION
-(
-	--@parametro TipoDato
-	@edad INT
-)
-RETURNS @Resultado TABLE
-(
-	Columna 1 tipoDato,
-	Columna 2 tipoDato,
-	Columna 3 tipoDato,
-)
-AS
-BEGIN
-	--Istrucciones SQL
-	RETURN;
-END;
-GO
-
 --Reservas de un cliente
 CREATE OR ALTER FUNCTION ADCR.fn_MT_ReservasClientes
 (
